16b2021bdb62ab894b901c2fc15519698512cb5f
堀内麻子 <>
2019-06-17 12:19:57 +0900

    wordpress準備ファイル

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo wp_get_document_title(); ?></title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    <?php wp_head(); ?>
+</head>
+<body>
+
+
+
+    
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+
+
+
+
+<?PHP get_footer(); ?>
